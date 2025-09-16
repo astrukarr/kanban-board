f5f5f04306e276c6fc25130b3cfafdcff148ccb6
AndelaS <astrukar@mybestbrands.de>
2025-09-16 21:21:34 +0200

    test: suppress 'Invalid token' console warnings in Jest

--- jest.setup.ts
+++ jest.setup.ts
@@ -8,3 +8,19 @@ Object.assign(global, {
   TextEncoder,
   TextDecoder,
 });
+
+// Mock console.warn to suppress expected "Invalid token" warnings in tests
+const originalWarn = console.warn;
+beforeAll(() => {
+  console.warn = jest.fn((message: string, ...args: unknown[]) => {
+    // Suppress "Invalid token" warnings in tests
+    if (typeof message === 'string' && message.includes('Invalid token')) {
+      return;
+    }
+    originalWarn(message, ...args);
+  });
+});
+
+afterAll(() => {
+  console.warn = originalWarn;
+});
